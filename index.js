b3d1ef83f13ce5054b2858d60c9e4ca1b7d630a8
Katrina R <katrina.reinsdorff@gmail.com>
2019-07-25 19:58:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,10 +16,10 @@ function happyHolidayTo (holiday, name) {
   return(`Happy ${holiday}, ${name}!`)
 }
 
-happyHolidayTo ("Independance Day", "you")
+happyHolidayTo ("Independance Day", "you");
 
 function holidayCountdown (holiday, days) {
   return (`It\'s ${days} days until ${holiday}!`)
 }
 
-holidayCountdown ("Mother's day", "20")
+holidayCountdown ("Mother's day", "20");
